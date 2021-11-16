fc11437be75489e90fb1967cf85fdc3eb5a6498f
norabraskerud <norabraskerud@gmail.com>
2021-11-16 08:21:05 +0100

    update

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-		"x-rapidapi-key": "916cb14e72msh92f3c6cac404df7p1f44b7jsne3fd7ba9d7b1"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+const API_URL = "https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1";
 
-const resultContainer = document.querySelector(".results");
+const options = { "headers": {
+    "x-rapidapi-key": "916cb14e72msh92f3c6cac404df7p1f44b7jsne3fd7ba9d7b1"
+}}
 
-async function fetchInfo() {
-    try { 
-        const response = await fetch(url);
-        const json = await response.json();
-
-        console.log(json);
-    }
-
+async function callApi() {
+    const response = await fetch(API_URL, options);
+    const results = await response.json();
+    console.log(results);
 }
 
-fetchInfo();
-
+callApi();
