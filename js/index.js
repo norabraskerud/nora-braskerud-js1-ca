be41dc8473d2224abb8415358bbe022388d496e7
norabraskerud <norabraskerud@gmail.com>
2021-11-17 19:50:02 +0100

    ad

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const API_URL = "https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1";
+const proxy = "https://noroffcors.herokuapp.com";
+
+const corsFix = proxy + URL;
 
 const options = { "headers": {
     "x-rapidapi-key": "916cb14e72msh92f3c6cac404df7p1f44b7jsne3fd7ba9d7b1"
 }}
+
+
 
 const resultsContainer = document.querySelector(".results");
 
@@ -12,7 +17,7 @@ async function callApi() {
         const results = await response.json();
         console.log(results);
 
-        createHtml(results);
+        createHtml(results.Search);
     }
     
     catch (error) {
@@ -26,8 +31,15 @@ callApi();
 
 function createHtml(results){
     for (let i = 0; i < results.length; i++) {
-        console.log(results[i]);
-        resultsContainer.innerHTML +=   `<div class="card"></div>
-                                        <h1 class="name">${results[i].title}</h1>`;
+        console.log(results[i])
+        resultsContainer.innerHTML +=   `<div class="card">
+                                        <h4 class="type">${results[i].Type}</h4>
+                                        <img scr="${results[i].Poster}"/>
+                                        <h4 class="name">${results[i].Title}</h4>
+                                        <h4 class="year">${results[i].Year}</h4>
+                                        </div>`
     }
 }
+
+    
+ 
